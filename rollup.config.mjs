347468482c0b4748adb6b5ecf4cab199c4fb1dfd
sagardwivedi <sagar.dw20@gmail.com>
2023-03-05 13:14:36 +0530

    commit

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -2,36 +2,30 @@ import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
-import postcss from "rollup-plugin-postcss";
-import tailwindcss from "tailwindcss";
-import autoprefixer from "autoprefixer";
+import packageJson from "./package.json" assert { type: "json" };
 
-import packageJson from "./package.json" assert {type: "json"};
+import postcss from "rollup-plugin-postcss"; //👈 new
 
 export default [
     {
         input: "src/index.ts",
         output: [
-            { file: packageJson.main, format: "cjs" },
-            { file: packageJson.module, format: "esm" }
+            { file: packageJson.main, format: "cjs", }, { file: packageJson.module, format: "esm", },
         ],
         plugins: [
             resolve(),
             commonjs(),
             typescript({ tsconfig: "./tsconfig.json" }),
             postcss({
-                plugins: [
-                    tailwindcss(),
-                    autoprefixer()
-                ],
-                extract: true,
-                minimize: true
+                plugins: []
             })
-        ]
+        ],
     },
     {
         input: "dist/esm/types/index.d.ts",
         output: [{ file: "dist/index.d.ts", format: "esm" }],
-        plugins: [dts()]
-    }
+        plugins: [dts()],
+        external: [/\.(css|less|scss)$/],
+    },
+
 ];
