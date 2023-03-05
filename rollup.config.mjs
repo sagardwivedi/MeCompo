0cba0a3b8f56554dca065177cb70aabc361b45f0
sagardwivedi <sagar.dw20@gmail.com>
2023-03-05 13:51:30 +0530

    added terser

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -2,9 +2,11 @@ import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
-import packageJson from "./package.json" assert { type: "json" };
+import postcss from "rollup-plugin-postcss";
+import terser from '@rollup/plugin-terser'
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
 
-import postcss from "rollup-plugin-postcss"; //👈 new
+import packageJson from "./package.json" assert { type: "json" };
 
 export default [
     {
@@ -13,12 +15,14 @@ export default [
             { file: packageJson.main, format: "cjs", }, { file: packageJson.module, format: "esm", },
         ],
         plugins: [
+            peerDepsExternal(),
             resolve(),
             commonjs(),
             typescript({ tsconfig: "./tsconfig.json" }),
             postcss({
                 plugins: []
-            })
+            }),
+            terser()
         ],
     },
     {
